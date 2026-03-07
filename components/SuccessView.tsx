5a6203bc5efb9c1f224b0e034015503146e6c98e
valigeriagioberti-glitch <valigeriagioberti@gmail.com>
2026-03-07 11:21:20 +0100

    Update SuccessView.tsx

--- components/SuccessView.tsx
+++ components/SuccessView.tsx
@@ -14,16 +14,6 @@ interface SuccessViewProps {
   language: Language;
 }
 
-useEffect(() => {
-  if (window.gtag) {
-    window.gtag('event', 'booking_completed', {
-      value: 1,
-      currency: 'EUR',
-      booking_ref: result.bookingRef || result.stripePaymentId
-    });
-  }
-}, [result]);
-
 export const SuccessView: React.FC<SuccessViewProps> = ({ result, onReset, language }) => {
   const t = TRANSLATIONS[language];
   const dateLocale = language === 'it' ? itLocale : enLocale;
